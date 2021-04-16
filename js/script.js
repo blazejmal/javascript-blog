a5bcc57800a33efa7e143b980c0214b44c56388f
Błażej Maluchnik <bmaluchnik03@gmail.com>
2021-04-16 19:08:42 +0200

    Code tideness

--- js/script.js
+++ js/script.js
@@ -1,55 +1,56 @@
 'use strict';
+{
+  const titleClickHandler = function (event) {
+    event.preventDefault();
+    const clickedElement = this;
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+    /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+  };
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+  // eslint-disable-next-line no-inner-declarations
+  function generateTitleLinks() {
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    /* for each article */
+    const articles = document.querySelector(optArticleSelector);
+    for (let article of articles) {
+      /* get the article id */
+      const articleId = document.getAttribute('id');
+      /* find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      /* get the title from the title element */
 
-const titleClickHandler = function (event) {
-  event.preventDefault();
-  const clickedElement = this;
-  console.log('Link was clicked!');
-
-
-  /* [DONE] remove class 'active' from all article links  */
-
-  const activeLinks = document.querySelectorAll('.titles a.active');
-
-  for (let activeLink of activeLinks) {
-    activeLink.classList.remove('active');
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log('linkHTML', linkHTML);
+      console.log('article', article);
+      /* insert link into titleList */
+    }
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-
-
-  clickedElement.classList.add('active');
-
-  /* console.log('clickedElement:', clickedElement); */
-
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post.active');
-
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
-  }
-  console.log(activeArticles);
-
-  /* get 'href' attribute from the clicked link */
-
-  const articleSelector = clickedElement.getAttribute('href');
-
-
-  console.log(articleSelector);
-  /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
-
-  console.log(targetArticle);
-
-  /* add class 'active' to the correct article */
-  targetArticle.classList.add('active');
-
+  generateTitleLinks();
 
 }
-
-const links = document.querySelectorAll('.titles a');
-
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
-}
-
-console.log("send to git");
